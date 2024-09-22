fd758f0c318d0840747187ce639215fca0d71342
Mttewk <mttewk@gmail.com>
2024-09-22 13:08:51 +0300

     операция сложения

--- TPproject/main.cpp
+++ TPproject/main.cpp
@@ -1,9 +1,10 @@
 #include <stdio.h>
 
 int main(void) {
-    int a, b;
+    int a, b, c;
     printf("Введите два числа: ");
     scanf("%d%d", &a, &b);
-    
+    c = a + b;
+    printf("%d\n", c);
     return 0;
 }
